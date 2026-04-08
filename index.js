ac90087e02b0f9ae9bb89d1d2faf7e0b6756323e
Siddhartha03112004 <siddharthasingh3112004@gmail.com>
2026-04-08 21:43:16 +0530

    Initial commit - Chat CRUD App

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const path = require("path");
+const Chat = require("./models/chat.js");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+// to cconect my static files such as style .css
+app.use(express.static(path.join(__dirname, "public")));
+
+//jo app.post ka data aata hai req.body ma
+// usse parse karna
+//hota hai ham ye line likhte hai
+app.use(express.urlencoded({ extended: true }));
+
+main()
+  .then(() => {
+    console.log("connection succesfull");
+  })
+  .catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
+}
+
+//index route
+app.get("/chats", async (req, res) => {
+  let chats = await Chat.find();
+  console.log(chats);
+  res.render("index.ejs", { chats });
+});
+
+//new route
+app.get("/chats/new", (req, res) => {
+  res.render("new.ejs");
+});
+
+//create route
+app.post("/chats", (req, res) => {
+  let { from, to, msg } = req.body;
+  let newChat = new Chat({
+    from: from,
+    to: to,
+    msg: msg,
+    created_at: new Date(),
+  });
+  newChat
+    .save()
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  res.redirect("/chats");
+});
+
+app.get("/", (req, res) => {
+  res.send("root is working");
+});
+
+app.listen(8080, () => {
+  console.log("server is listening on port 8080");
+});
---
+++ init.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const Chat = require("./models/chat.js");
+
+main()
+  .then(() => {
+    console.log("connection succesfull");
+  })
+  .catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
+}
+
+let allChats = [
+  {
+    from: "neha",
+    to: "priya",
+    msg: "send me your exam result",
+    created_at: new Date(),
+  },
+  {
+    from: "rahul",
+    to: "amit",
+    msg: "did you complete the assignment?",
+    created_at: new Date(),
+  },
+  {
+    from: "sneha",
+    to: "kavya",
+    msg: "let's meet after college",
+    created_at: new Date(),
+  },
+  {
+    from: "arjun",
+    to: "rohit",
+    msg: "bro send me the notes please",
+    created_at: new Date(),
+  },
+];
+Chat.insertMany(allChats);
+
+// chat1.save().then((res) => {
+//   //utc time format
+//   console.log(res);
+// });
