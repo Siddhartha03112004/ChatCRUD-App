059b7029670334fc60acef849f8e70d1eff270b0
Siddhartha03112004 <siddharthasingh3112004@gmail.com>
2026-04-09 20:02:17 +0530

    Added update & delete routes + improvements

--- index.js
+++ index.js
@@ -1,43 +1,45 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const mongoose = require("mongoose");
-const path = require("path");
-const Chat = require("./models/chat.js");
+const mongoose = require('mongoose');
+const path = require('path');
+const Chat = require('./models/chat.js');
+const methodOverride = require('method-override');
 
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 // to cconect my static files such as style .css
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 //jo app.post ka data aata hai req.body ma
 // usse parse karna
 //hota hai ham ye line likhte hai
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 main()
   .then(() => {
-    console.log("connection succesfull");
+    console.log('connection succesfull');
   })
   .catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
+  await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
 }
 
 //index route
-app.get("/chats", async (req, res) => {
+app.get('/chats', async (req, res) => {
   let chats = await Chat.find();
-  console.log(chats);
-  res.render("index.ejs", { chats });
+  // console.log(chats);
+  res.render('index.ejs', { chats });
 });
 
 //new route
-app.get("/chats/new", (req, res) => {
-  res.render("new.ejs");
+app.get('/chats/new', (req, res) => {
+  res.render('new.ejs');
 });
 
 //create route
-app.post("/chats", (req, res) => {
+app.post('/chats', (req, res) => {
   let { from, to, msg } = req.body;
   let newChat = new Chat({
     from: from,
@@ -53,13 +55,42 @@ app.post("/chats", (req, res) => {
     .catch((err) => {
       console.log(err);
     });
-  res.redirect("/chats");
+  res.redirect('/chats');
 });
 
-app.get("/", (req, res) => {
-  res.send("root is working");
+//edit route
+
+app.get('/chats/:id/edit', async (req, res) => {
+  let { id } = req.params;
+  let chat = await Chat.findById(id);
+  res.render('edit.ejs', { chat });
+});
+
+app.get('/', (req, res) => {
+  res.send('root is working');
+});
+
+//update route
+app.put('/chats/:id', async (req, res) => {
+  let { id } = req.params;
+  let { msg: newMsg } = req.body;
+  let updatedChat = await Chat.findByIdAndUpdate(
+    id,
+    { msg: newMsg },
+    { runValidators: true, new: true },
+  );
+
+  res.redirect('/chats');
+});
+
+//destroy route
+
+app.delete('/chats/:id', async (req, res) => {
+  let { id } = req.params;
+  let deletedChat = await Chat.findByIdAndDelete(id);
+  res.redirect('/chats');
 });
 
 app.listen(8080, () => {
-  console.log("server is listening on port 8080");
+  console.log('server is listening on port 8080');
 });
